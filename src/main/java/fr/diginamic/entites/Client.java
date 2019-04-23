8302c244c72f5d22c620292f4f2413e1cb393955
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-04-23 10:38:41 +0200

    TP 1.2

---
+++ src/main/java/fr/diginamic/entites/Client.java
@@ -0,0 +1,14 @@
+package fr.diginamic.entites;
+
+public class Client {
+	
+	int numeroCompte;
+	String nom;
+	String prenom;
+	
+	public Client(int compte, String name, String nickname) {
+		
+		System.out.println("Nom " + name + " Prénom " + nickname + " Numéro de compte " + compte);		
+	}
+
+}
---
+++ src/main/java/fr/diginamic/essais/TestClient.java
@@ -0,0 +1,14 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.entites.Client;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+
+		Client cli1 = new Client(15, "Kharbeche", "Bilel");
+		Client cli2 = new Client(55, "Beninca", "Aurélie");
+
+	}
+
+}
