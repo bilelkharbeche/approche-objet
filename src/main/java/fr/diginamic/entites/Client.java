2f6cdbdf4a9f3b12608732e6ecb86782825009f3
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-04-23 11:20:58 +0200

    TP 1.2 FINAL F

--- src/main/java/fr/diginamic/entites/Client.java
+++ src/main/java/fr/diginamic/entites/Client.java
@@ -1,18 +1,18 @@
 package fr.diginamic.entites;
 
 public class Client {
-	
+
 	public int numeroCompte;
 	public String nom;
 	public String prenom;
-	
+
 	public Client(int compte, String name, String nickname) {
-		
+
 		numeroCompte = compte;
 		nom = name;
 		prenom = nickname;
-		
-		System.out.println("Nom " + name + " Prénom " + nickname + " Numéro de compte " + compte);		
+
+		System.out.println("Nom " + nom + " Prénom " + prenom + " Numéro de compte " + numeroCompte);
 	}
 
 }
--- src/main/java/fr/diginamic/essais/TestClient.java
+++ src/main/java/fr/diginamic/essais/TestClient.java
@@ -7,8 +7,8 @@ public class TestClient {
 	public static void main(String[] args) {
 
 		Client cli1 = new Client(15, "Kharbeche", "Bilel");
-		Client cli2 = new Client(55, "Beninca", "Aurélie");
-		
+		Client cli2 = new Client(55, "B", "Aurélie");
+
 		System.out.println("\n" + cli1.nom);
 		System.out.println(cli2.numeroCompte);
 
